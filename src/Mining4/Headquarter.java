cea2e05ad82f0cc15a64ff6c32982eed1d9cea34
kyleezz <kylee.zhang2@gmail.com>
2023-01-12 02:04:06 +1100

    mining

---
+++ src/Mining4/Headquarter.java
@@ -0,0 +1,132 @@
+package Mining4;
+
+import battlecode.common.*;
+
+public class Headquarter extends Robot {
+
+    /**
+     * Run a single turn for a Headquarters.
+     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+     */
+
+//    static Direction globalDir = Direction.CENTER;
+
+    public Headquarter(RobotController rc) throws GameActionException {
+        super(rc);
+    }
+
+    public void init() {
+
+    }
+
+    static int carrierCount = 0;
+
+    static MapLocation getClosestMine() {
+        WellInfo[] wells = rc.senseNearbyWells();
+
+        // IS THIS A BUG ?????
+        // IS THIS A BUG ?????
+        // IS THIS A BUG ?????
+        // IS THIS A BUG ?????
+        // IS THIS A BUG ?????
+        // IS THIS A BUG ?????
+
+//        if (wells.length == 0) {
+//            System.out.println(rc.getType() + " AINT NOOOW AYYYY");
+//        }
+
+        int closestDist = 1_000_000, closestIdx = -1;
+
+        for (int i = 0; i < wells.length; i++) {
+            // guaranteed to be one
+            int dist = wells[i].getMapLocation().distanceSquaredTo(rc.getLocation());
+            if (dist < closestDist) {
+                closestDist = dist;
+                closestIdx = i;
+            }
+        }
+
+        return wells[closestIdx].getMapLocation();
+    }
+
+    public void runUnit() throws GameActionException {
+//        if (rc.getRoundNum() % 80 == 1 && rc.readSharedArray(0)==0) {
+//            if (rc.getResourceAmount(ResourceType.MANA) >= 40 && rc.getResourceAmount(ResourceType.ADAMANTIUM) >= 40) {
+//                Direction dir; MapLocation newLoc;
+//                do {
+//                    dir = directions[rng.nextInt(directions.length)];
+//                    newLoc = rc.getLocation().add(dir);
+//                } while (!rc.canBuildRobot(RobotType.AMPLIFIER, newLoc));
+//                rc.setIndicatorString("Trying to build a amplifier");
+//                rc.buildRobot(RobotType.AMPLIFIER, newLoc);
+//                return;
+////                globalDir = dir;
+//            }
+//        }
+        if (rc.canBuildAnchor(Anchor.STANDARD) && rc.getRoundNum() >= 100) {
+            // If we can build an anchor do it!
+            rc.buildAnchor(Anchor.STANDARD);
+            rc.setIndicatorString("Building anchor! " + rc.getAnchor());
+            return;
+        }
+
+        RobotType toMake = null;
+
+        if (rc.getResourceAmount(ResourceType.MANA)-rc.getResourceAmount(ResourceType.ADAMANTIUM) >= 60) {
+            toMake = RobotType.LAUNCHER;
+        }
+
+        if (rc.getResourceAmount(ResourceType.ADAMANTIUM) >= 50 && (carrierCount < 40 || rc.getAnchor() != null) && (toMake == null || rng.nextInt(2) == 1)) {
+            toMake = RobotType.CARRIER;
+            carrierCount++;
+        }
+
+        if (toMake != null) {
+            Direction dir; MapLocation newLoc;
+
+            do {
+                dir = directions[rng.nextInt(directions.length)];
+                newLoc = rc.getLocation().add(dir);
+            } while (!rc.canBuildRobot(toMake, newLoc));
+
+            rc.buildRobot(toMake, newLoc);
+        }
+    }
+}
+
+
+
+// DONT REMOVE YET
+
+//            if (rc.getRoundNum() >= 2 && rc.getRoundNum() <= 50) {
+//                // spwan it close to the mine
+//
+//                MapLocation ideal = getClosestMine();
+//                boolean found = false;
+//
+//                int closest = 1_000_000;
+//                MapLocation closestLoc = null;
+//
+//                for (int dx = -3; dx <= 3; dx++ ) {
+//                    for (int dy = -3; dy <= 3; dy++ ) {
+//                        if (dx * dx + dy * dy > 9) continue;
+//                        MapLocation temp = new MapLocation(ideal.x + dx, ideal.y + dy);
+//                        if (rc.onTheMap(temp) && rc.canBuildRobot(RobotType.CARRIER, temp) && temp.distanceSquaredTo(ideal) < closest) {
+//                            System.out.println(closest + " " + temp + " " + ideal);
+//                            closest = temp.distanceSquaredTo(ideal);
+//                            closestLoc = temp;
+//                            found = true;
+//                        }
+//                    }
+//                }
+//                if (found) {
+//                    newLoc = closestLoc;
+//                } else {
+//                    // eh just do it anywhere
+//                    do {
+//                        dir = directions[rng.nextInt(directions.length)];
+//                        newLoc = rc.getLocation().add(dir);
+//                    } while (!rc.canBuildRobot(RobotType.CARRIER, newLoc));
+//                }
+//
+//            } else {
---
+++ src/Mining4/Launcher.java
@@ -0,0 +1,123 @@
+package Mining4;
+
+import battlecode.common.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+public class Launcher extends Robot {
+    static MapLocation parentLoc = null;
+    ArrayList<Integer> randomPermutation = new ArrayList<>();
+
+    static boolean attacking = false;
+    public Launcher(RobotController rc) throws GameActionException {
+        super(rc);
+        for (int i = 0; i < 90; i++) {
+            randomPermutation.add(i);
+        }
+
+        Collections.shuffle(randomPermutation);
+
+        if (rng.nextInt(4) != 0) {
+            attacking = true;
+        }
+    }
+    public void init() throws GameActionException {
+    }
+    public void runUnit() throws GameActionException {
+
+        System.out.println("bruh");
+        if (parentLoc == null) {
+            RobotInfo[] friends = rc.senseNearbyRobots(42069,rc.getTeam());
+            int mini=42069;
+            for (RobotInfo friend : friends) {
+                if (friend.type == RobotType.HEADQUARTERS) {
+                    if (rc.getLocation().distanceSquaredTo(friend.location)<mini) {
+                        mini = rc.getLocation().distanceSquaredTo(friend.location);
+                        parentLoc = friend.location;
+                    }
+                }
+            }
+        }
+
+        // Try to attack someone
+        int radius = rc.getType().actionRadiusSquared;
+        Team opponent = rc.getTeam().opponent();
+        RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
+        if (enemies.length > 0) {
+            for (RobotInfo enemy : enemies) {
+                MapLocation toAttack = enemy.location;
+                if (rc.canAttack(toAttack)) {
+                    rc.attack(toAttack);
+                    break;
+                }
+            }
+        }
+
+        if (true) {
+            moveRandom();
+        } else {
+
+            // Location to guard
+            MapLocation toGuard = null;
+
+            for (int i = 0; i < 100; i++) {
+                if (randomPermutation.get(i) <= 59) {
+                    // it's an island
+                    // check .get(i) / 2
+                    int current = (randomPermutation.get(i)) / 2 + 1;
+                    int comm = rc.readSharedArray(current);
+
+                    if (comm != 0) {
+                        comm--;
+                        int x = comm / 69, y = comm % 69;
+                        toGuard = new MapLocation(x, y);
+                        break;
+                    }
+                } else {
+                    // assign to wells
+                    int current = (randomPermutation.get(i) - 10);
+
+                    if (current > 63) continue;
+
+                    // [50, 63]
+
+                    int comm = rc.readSharedArray(current);
+                    if (comm != 0) {
+                        comm--;
+                        int x = comm / 69, y = comm % 69;
+                        toGuard = new MapLocation(x, y);
+                        break;
+                    }
+                }
+            }
+
+            if (toGuard != null) {
+                rc.setIndicatorString("I am guarding " + toGuard);
+                if (rc.getLocation().distanceSquaredTo(toGuard) <= 5) {
+                    // Destination + (3, 0)
+
+                    Direction targetDirection = rc.getLocation().directionTo(toGuard).rotateRight().rotateRight();
+                    if (targetDirection == Direction.CENTER) {
+                        targetDirection = Direction.NORTH;
+                    }
+                    Direction[] possible = new Direction[5];
+                    for (int i = 0; i < 5; i++) {
+                        possible[i] = targetDirection;
+                        targetDirection = targetDirection.rotateRight();
+                    }
+
+                    moveToLocation(rc.getLocation().add(possible[rng.nextInt(possible.length)]));
+                } else{
+                    moveToLocation(toGuard);
+                }
+            } else {
+                moveRandom();
+            }
+
+
+        }
+
+
+
+    }
+}
