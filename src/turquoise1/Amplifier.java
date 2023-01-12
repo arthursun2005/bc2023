3494c8b0255d350fb3a6964b0f5fa6b3d6a488e1
Arthur <arthursun2005@gmail.com>
2023-01-13 06:36:11 +1100

    turq

---
+++ src/turquoise1/Amplifier.java
@@ -0,0 +1,17 @@
+package turquoise1;
+
+import battlecode.common.*;
+
+// HUGELY TODO
+
+public class Amplifier extends Robot
+{
+    public Amplifier(RobotController rc) throws GameActionException
+    {
+        super(rc);
+    }
+
+    public void run()
+    {
+    }
+}
