65f9855c2a0b4a165a56323a329b45a1d7828b99
ziruili <zirui.li7@gmail.com>
2023-01-13 21:01:21 +1100

    sussy

---
+++ src/bingchilling3/Amplifier.java
@@ -0,0 +1,17 @@
+package bingchilling3;
+
+import battlecode.common.*;
+
+// HUGELY TODO
+
+public class Amplifier extends Robot
+{
+    public Amplifier(RobotController rc) throws GameActionException
+    {
+        super(rc);
+    }
+
+    public void run()
+    {
+    }
+}
