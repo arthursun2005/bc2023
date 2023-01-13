2758b7ad5238f5e443473096008698b565103baa
kyleezz <kylee.zhang2@gmail.com>
2023-01-13 15:20:56 +1100

    useless tbh

---
+++ src/Mining0/Robot.java
@@ -0,0 +1,222 @@
+package Mining0;
+
+import battlecode.common.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Random;
+
+public abstract class Robot {
+    static Random rng;
+
+    /** Array containing all the possible movement directions. */
+    static final Direction[] directions = {
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST,
+    };
+
+    static int moveCount = 0;
+    static Direction curDir = Direction.NORTH;
+    static RobotController rc;
+
+    static Communication communication;
+    static int turnCount = 0;
+    static int currentRound;
+
+    static int hqCount = 0;
+    static boolean calcHQ = false;
+    static Movement movement;
+    static MapLocation prevLocation = null;
+    static Direction prevDirection = null;
+
+    static ArrayList<MapLocation> HQLocations = new ArrayList<>();
+    // WELL FINDING
+    static WellUtility wellUtility;
+
+// END WELL FINDING
+
+    public Robot(RobotController rc) {
+        Robot.rc = rc;
+        rng = new Random(rc.getID());
+
+        wellUtility = new WellUtility(rc);
+        communication = new Communication(rc);
+    }
+
+    abstract void init() throws GameActionException;
+    public void run() throws GameActionException {
+        // Before unit runs
+        turnCount++;
+        currentRound = rc.getRoundNum();
+
+        if (prevLocation == null) {
+            prevLocation = rc.getLocation();
+        } else if (!rc.getLocation().equals(prevLocation)) {
+            prevDirection = prevLocation.directionTo(rc.getLocation());
+            prevLocation = rc.getLocation();
+        }
+
+        if (rc.getType().equals(RobotType.CARRIER)) {
+            wellUtility.senseWells();
+        }
+
+        if (rc.getType().equals(RobotType.HEADQUARTERS) && rc.getRoundNum() == 1) {
+            for (int i = 40; i < 50; i++) {
+                if (rc.readSharedArray(i) != 0) continue;
+                rc.writeSharedArray(i, rc.getLocation().x * 69 + rc.getLocation().y);
+                break;
+            }
+            wellUtility.senseWells();
+        }
+
+        if (!calcHQ && rc.canWriteSharedArray(0, 0)) {
+            calcHQ = true;
+            for (int i = 40; i < 50; i++) {
+                if (rc.readSharedArray(i) != 0) {
+                    int decode = rc.readSharedArray(i);
+                    hqCount++;
+                    HQLocations.add(new MapLocation(decode / 69, decode % 69));
+                }
+            }
+        }
+
+
+        // Robot init
+        init();
+        // Does turn
+        runUnit();
+
+        if (Clock.getBytecodesLeft() >= 500) {
+            wellUtility.senseWells();
+        }
+    }
+
+    abstract void runUnit() throws GameActionException;
+
+    MapLocation nearestHQ() {
+        Collections.sort(HQLocations, new Comparator<MapLocation>() {
+            public int compare(MapLocation a, MapLocation b) {
+                return rc.getLocation().distanceSquaredTo(a) - rc.getLocation().distanceSquaredTo(b);
+            }
+        });
+
+        return HQLocations.get(0);
+    }
+    public void moveTo(MapLocation loc) throws GameActionException
+    {
+        if (rc.isMovementReady()) {
+            Direction dir = movement.tryMove(rc, loc, prevDirection);
+            tryMove(dir);
+        }
+    }
+    public void moveToLocation(MapLocation loc) throws GameActionException {
+        // CHECK IF MOVE IS VALID
+
+        if (rc.isMovementReady()) {
+            Direction dir = movement.tryMove(rc, loc, prevDirection);
+            if (dir != null && rc.canMove(dir)) {
+                rc.move(dir);
+            } else return;
+        }
+    }
+
+    public void moveToLocationRepeat(MapLocation loc) throws GameActionException {
+        // CHECK IF MOVE IS VALID
+
+        while (rc.isMovementReady()) {
+            Direction dir = movement.tryMove(rc, loc, prevDirection);
+            if (dir != null && rc.canMove(dir)) {
+                rc.move(dir);
+            } else return;
+        }
+    }
+
+    static void moveRandom() throws GameActionException {
+        if (rc.isMovementReady()) {
+            if (moveCount==0 || !rc.canMove(curDir)) {
+                moveCount=4;
+                Direction newDirects[] = {
+                        curDir,
+                        curDir.rotateRight(),
+                        curDir.rotateLeft(),
+                        curDir.rotateRight().rotateRight(),
+                        curDir.rotateLeft().rotateLeft(),
+                };
+                for (int i=0; i<20; i++) {
+                    curDir = newDirects[rng.nextInt(newDirects.length)];
+                    if (rc.canMove(curDir)) {
+                        rc.move(curDir);
+                        return;
+                    }
+                }
+                while (true) {
+                    curDir = directions[rng.nextInt(directions.length)];
+                    if (rc.canMove(curDir)) {
+                        rc.move(curDir);
+                        return;
+                    }
+                }
+            }
+            moveCount--;
+            rc.move(curDir);
+        }
+    }
+    public boolean tryMove(Direction dir) throws GameActionException
+    {
+        boolean moved = false;
+        if (dir == null) return false;
+        while (rc.canMove(dir))
+        {
+            moved = true;
+            rc.move(dir);
+        }
+
+        return moved;
+    }
+
+    public void spreadOut(boolean all) throws GameActionException
+    {
+        // move in the direction with the least friendly robots
+        // if all is true, consider robots of every type instead of just your own
+
+        int cnt[] = new int[9];
+        RobotInfo[] robots = rc.senseNearbyRobots(-1, rc.getTeam());
+        for (RobotInfo ri : robots)
+        {
+            if (all || ri.type.equals(rc.getType()))
+            {
+                Direction dir = rc.getLocation().directionTo(ri.getLocation());
+                cnt[dir.ordinal()]++;
+            }
+        }
+        int least = 696969;
+        for (int i = 0; i < 8; i++)
+        {
+            least = Math.min(least, cnt[i]);
+        }
+        ArrayList<Direction> dirs = new ArrayList<Direction>();
+        for (int i = 0; i < 8; i++)
+        {
+            if (cnt[i] == least)
+            {
+                dirs.add(directions[i]);
+            }
+        }
+        Collections.shuffle(dirs);
+        for (Direction dir : dirs)
+        {
+            if (tryMove(dir))
+            {
+                break;
+            }
+        }
+    }
+}
+
---
+++ src/Mining0/RobotPlayer.java
@@ -0,0 +1,91 @@
+package Mining0;
+
+import battlecode.common.Clock;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+
+/**
+ * RobotPlayer is the class that describes your main robot strategy.
+ * The run() method inside this class is like your main function: this is what we'll call once your robot
+ * is created!
+ */
+public strictfp class RobotPlayer {
+
+    /**
+     * We will use this variable to count the number of turns this robot has been alive.
+     * You can use static variables like this to save any information you want. Keep in mind that even though
+     * these variables are static, in Battlecode they aren't actually shared between your robots.
+     */
+    static MapLocation minLoc = null;
+
+    /**
+     * A random number generator.
+     * We will use this RNG to make some random moves. The Random class is provided by the java.util.Random
+     * import at the top of this file. Here, we *seed* the RNG with a constant number (6147); this makes sure
+     * we get the same sequence of numbers every time this code is run. This is very useful for debugging!
+     */
+
+
+    /**
+     * run() is the method that is called when a robot is instantiated in the Battlecode world.
+     * It is like the main function for your robot. If this method returns, the robot dies!
+     *
+     * @param rc  The RobotController object. You use it to perform actions from this robot, and to get
+     *            information on its current status. Essentially your portal to interacting with the world.
+     **/
+    @SuppressWarnings("unused")
+    public static void run(RobotController rc) throws GameActionException {
+
+        Robot robot = null;
+
+        // Hello world! Standard output is very useful for debugging.
+        // Everything you say here will be directly viewable in your terminal when you run a match!
+        // System.out.println("I'm a " + rc.getType() + " and I just got created! I have health " + rc.getHealth());
+
+        // You can also use indicators to save debug notes in replays.
+        // rc.setIndicatorString("Hello world!");
+
+        switch (rc.getType()) {
+            case HEADQUARTERS:
+                robot = new Headquarter(rc);
+                break;
+            case CARRIER:
+                robot = new Carrier(rc);
+                break;
+            case LAUNCHER:
+                robot = new Launcher(rc);
+                break;
+            case BOOSTER:
+                robot = null;
+                break;
+            case DESTABILIZER:
+                robot = null;
+                break;
+            case AMPLIFIER:
+                robot = new Amplifier(rc);
+                break;
+        }
+
+        while (true) {
+            try {
+                if (robot != null) {
+                    robot.run();
+                }
+            } catch (GameActionException e) {
+                System.out.println(rc.getType() + " Exception");
+                e.printStackTrace();
+
+            } catch (Exception e) {
+                System.out.println(rc.getType() + " Exception");
+                e.printStackTrace();
+            } finally {
+                Clock.yield();
+            }
+        }
+
+    }
+
+
+
+}
