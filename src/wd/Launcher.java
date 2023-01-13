103bfaf8780849e1446e7740624d80dfb7becc8f
kyleezz <kylee.zhang2@gmail.com>
2023-01-13 23:29:27 +1100

    WDD

---
+++ src/wd/Launcher.java
@@ -0,0 +1,172 @@
+package wd;
+
+import battlecode.common.*;
+
+public class Launcher extends Robot
+{
+    public Launcher(RobotController rc) throws GameActionException
+    {
+        super(rc);
+    }
+
+    public boolean tryAttack() throws GameActionException
+    {
+        int radius = rc.getType().actionRadiusSquared;
+        Team opponent = rc.getTeam().opponent();
+        RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
+        MapLocation attackLoc = null;
+        int minHealth = -1;
+        int theID = -1;
+        for (RobotInfo enemy : enemies) {
+            MapLocation toAttack = enemy.location;
+            if (rc.canAttack(toAttack)) {
+                int adjustedHealth = enemy.getHealth() * 123456 + enemy.getID();
+                if (enemy.type.equals(RobotType.LAUNCHER)) adjustedHealth -= 123456789;
+                if (minHealth == -1 || adjustedHealth < minHealth)
+                {
+                    minHealth = adjustedHealth;
+                    theID = enemy.getID();
+                    attackLoc = toAttack;
+                }
+            }
+        }
+        if (attackLoc != null)
+        {
+            rc.attack(attackLoc);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean tryChaseOrRetreat() throws GameActionException
+    {
+        Team opponent = rc.getTeam().opponent();
+        RobotInfo[] friends = rc.senseNearbyRobots(-1, rc.getTeam());
+        RobotInfo[] enemies = rc.senseNearbyRobots(-1, opponent);
+        int friendOffensiveCnt = 5 + rc.getHealth();
+        int enemyOffensiveCnt = 0;
+        MapLocation weakLoc = null;
+        int minHealth = -1;
+        for (RobotInfo friend : friends)
+        {
+            if (friend.type.equals(RobotType.LAUNCHER))
+            {
+                friendOffensiveCnt += 5 + friend.health;
+            }
+        }
+        for (RobotInfo enemy : enemies)
+        {
+            if (enemy.type.equals(RobotType.HEADQUARTERS)) continue;
+            if (enemy.type.equals(RobotType.LAUNCHER))
+            {
+                enemyOffensiveCnt += 5 + enemy.health;
+            }
+
+            int adjustedHealth = enemy.getHealth() * 123456 + enemy.getID();
+            if (enemy.type.equals(RobotType.LAUNCHER)) adjustedHealth -= 123456789;
+            if (minHealth == -1 || adjustedHealth < minHealth)
+            {
+                minHealth = adjustedHealth;
+                weakLoc = enemy.getLocation();
+            }
+        }
+
+        if (enemyOffensiveCnt > friendOffensiveCnt + 5)
+        {
+            // retreat
+            moveTo(tracker.getClosestHQLoc());
+        }else if (friendOffensiveCnt > enemyOffensiveCnt + 5) {
+            // attack
+            if (weakLoc != null)
+            {
+                moveTo(weakLoc);
+            }
+        }
+
+        if (enemies.length == 1 && enemies[0].type.equals(RobotType.HEADQUARTERS) && rc.getID() % 5 != 0) return false;
+        return enemies.length > 0;
+    }
+
+    public void tryProtect() throws GameActionException
+    {
+        RobotInfo[] friends = rc.senseNearbyRobots(-1, rc.getTeam());
+        MapLocation HQLoc = tracker.getClosestHQLoc();
+        int dist = rc.getLocation().distanceSquaredTo(HQLoc);
+        MapLocation weakLoc = null;
+        MapLocation center = new MapLocation(rc.getMapWidth() / 2, rc.getMapHeight() / 2);
+        for (RobotInfo friend : friends)
+        {
+            int w = friend.getLocation().distanceSquaredTo(center);
+            if (friend.type.equals(RobotType.CARRIER))
+            {
+                // w -= 123456789;
+            }
+            if (w < dist)
+            {
+                dist = w;
+                weakLoc = friend.getLocation();
+            }
+        }
+
+        if (weakLoc != null)
+        {
+            moveTo(weakLoc);
+        }
+    }
+
+    static boolean crossed = false;
+
+    public void run() throws GameActionException
+    {
+        tryAttack();
+        if (tryChaseOrRetreat())
+        {
+            tryAttack();
+            return;
+        }
+
+        RobotInfo[] friends = rc.senseNearbyRobots(42069,rc.getTeam());
+        int mini=rc.getID();
+        MapLocation bestie = null;
+        int lowerCount = 0;
+
+        for (RobotInfo friend : friends) {
+            if (friend.type == RobotType.LAUNCHER) {
+                if (friend.getID() < rc.getID()) lowerCount++;
+                if (friend.getID() < mini) {
+                    mini=friend.getID();
+                    bestie=friend.getLocation();
+                }
+            }
+        }
+
+        MapLocation oppositeLoc = new MapLocation(rc.getMapWidth()-parentLoc.x-1,rc.getMapHeight()-parentLoc.y-1);
+        if (rc.getLocation().distanceSquaredTo(oppositeLoc)*2<=rc.getLocation().distanceSquaredTo(parentLoc)) {
+            crossed = true;
+        }
+
+        if (mini < rc.getID() && lowerCount < 9) {
+            if (rc.getLocation().distanceSquaredTo(bestie) > 2)
+            {
+                moveTo(bestie);
+            }
+        }
+        else {
+            if (!crossed) {
+                if (rc.getRoundNum() % 2 == 0)
+                {
+                    moveTo(oppositeLoc);
+                }
+            }else{
+                moveRandom();
+            }
+        }
+        // tryProtect();
+
+        /*MapLocation center = new MapLocation(rc.getMapWidth() / 2, rc.getMapHeight() / 2);
+        moveTo(center);
+
+        spreadOut(true);*/
+        tryAttack();
+    }
+}
