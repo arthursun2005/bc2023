ecbfa5de4226ce1974d98cb7e95dd25ab2694892
arthur <arthur@snorlax>
2023-01-17 04:04:43 +1100

    eg

---
+++ src/eggnog/Booster.java
@@ -0,0 +1,18 @@
+package eggnog;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+// HUGELY TODO
+
+public class Booster extends Robot
+{
+    public Booster(RobotController rc) throws GameActionException
+    {
+        super(rc);
+    }
+
+    public void run()
+    {
+    }
+}
