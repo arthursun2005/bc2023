2e415cc1d7374bb132ff8f20db6c90b473705584
kyleezz <kylee.zhang2@gmail.com>
2023-01-17 10:48:47 +1100

    bruh

---
+++ src/eggnogtest/CustomWell.java
@@ -0,0 +1,22 @@
+package eggnogtest;
+
+import battlecode.common.MapLocation;
+import battlecode.common.ResourceType;
+
+public class CustomWell {
+    MapLocation location;
+    ResourceType resourceType;
+
+    public CustomWell(MapLocation loc, ResourceType type) {
+        location = loc;
+        resourceType = type;
+    }
+
+    public MapLocation getMapLocation() {
+        return location;
+    }
+
+    public ResourceType getResourceType() {
+        return resourceType;
+    }
+}
