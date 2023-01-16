4658a9891f3cf00e4b02ccf73c0d4c7b6e761444
ziruili <zirui.li7@gmail.com>
2023-01-17 04:33:14 +1100

    pls mf

---
+++ src/bonknogg/Amplifier.java
@@ -0,0 +1,18 @@
+package bonknogg;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+// HUGELY TODO
+
+public class Amplifier extends Robot
+{
+    public Amplifier(RobotController rc) throws GameActionException
+    {
+        super(rc);
+    }
+
+    public void run()
+    {
+    }
+}
--- src/bonknogg/Launcher.java
+++ src/bonknogg/Launcher.java
@@ -1,4 +1,4 @@
-package eggnog;
+package bonknogg;
 
 import battlecode.common.*;
 
@@ -6,6 +6,79 @@ import java.util.*;
 
 public class Launcher extends Robot
 {
+    static long arr[] = new long[195]; // 2^63 - 1
+    // 3 per location, 21 locations in one long, need 180
+
+    static void put(int loc, int val) {
+        arr[loc / 19] |= ((long) val << ((loc % 19) * 3));
+    }
+
+    static int read(int loc) {
+        return (int) ((arr[loc / 19] >> ((loc % 19)*3))%8);
+    }
+    static int possi[] = {0,1,1,1};
+
+    static boolean foundSymmetry = false;
+
+    static int symmetry = 0;
+
+    static int encode(int x, int y) {
+        return x * 60 + y;
+    }
+
+    static RobotInfo possibleHQ = null;
+    static MapInfo mapInfo = null;
+    static int nn,no,on,oo;
+
+    static void tryFindSymmetry() throws GameActionException {
+        if (!foundSymmetry) {
+            int radius = 2;
+
+            MapLocation toCheck[] = rc.getAllLocationsWithinRadiusSquared(rc.getLocation(),radius);
+            int x, y, oppx, oppy;
+            MapLocation loc = null;
+            // 1 for passable, 2 for impassable
+            for (int i = 0; i < toCheck.length; i++) {
+                loc = toCheck[i];
+
+                if (!rc.canSenseLocation(loc)) continue; // 5
+
+                x = loc.x; y = loc.y;
+                oppx = rc.getMapWidth()-x-1; oppy = rc.getMapHeight()-y-1;
+
+                possibleHQ = rc.senseRobotAtLocation(loc); // 25
+                mapInfo = rc.senseMapInfo(loc); // 10
+
+                if (possibleHQ != null && possibleHQ.getType().equals(RobotType.HEADQUARTERS)) {
+                    put(encode(x, y), 1);
+                } else if (!mapInfo.getCurrentDirection().equals(Direction.CENTER)) {
+                    put(encode(x, y), 2);
+                } else if (mapInfo.hasCloud()) {
+                    put(encode(x, y), 3);
+                } else if (rc.sensePassability(loc)) {
+                    put(encode(x, y), 4);
+                } else {
+                    put(encode(x, y), 5);
+                }
+
+                nn=read(encode(x, y));
+                on=read(encode(oppx, y));
+                no=read(encode(x, oppy));
+                oo=read(encode(oppx, oppy));
+
+                if (on != 0 && on != nn) possi[1] = 0;
+                if (no != 0 && no != nn) possi[2] = 0;
+                if (oo != 0 && oo != nn) possi[3] = 0;
+            }
+
+            if (possi[1]+possi[2]+possi[3]==1) {
+                rc.setIndicatorString("SYMMETRY FOUND" + (possi[1] == 1 ? 1 : (possi[2] == 1 ? 2 : 3)));
+                symmetry = (possi[1] == 1 ? 1 : (possi[2] == 1 ? 2 : 3));
+                foundSymmetry = true;
+            }
+        }
+    }
+
     public Launcher(RobotController rc) throws GameActionException
     {
         super(rc);
@@ -323,7 +396,9 @@ public class Launcher extends Robot
 
     public void run() throws GameActionException
     {
+        if (turnCount >= 1) tryFindSymmetry();
         turnCount++;
+
         MapLocation ga = getAttackLoc();
         Direction bd = getChaseOrRetreatDir();
 
