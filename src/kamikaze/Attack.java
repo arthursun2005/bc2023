8ca2adf9d4e23858e0cff0c2c8c48475bd1ccb75
arthur <arthur@snorlax>
2023-01-17 20:29:12 +1100

    cleanup

---
+++ src/kamikaze/Attack.java
@@ -0,0 +1,11 @@
+package kamikaze;
+
+import battlecode.common.*;
+
+public class Attack {
+    RobotController rc;
+    
+    public Attack(RobotController rc) throws GameActionException {
+        this.rc = rc;
+    }
+}
