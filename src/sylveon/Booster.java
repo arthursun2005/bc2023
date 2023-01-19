e82c883f55b07c274e11f2182e03d39d4469fc74
arthur <arthur@snorlax>
2023-01-19 11:07:55 +1100

    sylv

---
+++ src/sylveon/Booster.java
@@ -0,0 +1,15 @@
+package sylveon;
+
+import battlecode.common.*;
+
+public class Booster extends Robot
+{
+    public Booster(RobotController rc) throws GameActionException
+    {
+        super(rc);
+    }
+
+    public void run()
+    {
+    }
+}
