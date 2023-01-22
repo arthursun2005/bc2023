f1842df32cd41fcde3ca68f423d192afc5c66a0b
ziruili <zirui.li7@gmail.com>
2023-01-22 19:11:02 +1100

    fml

---
+++ src/buggycurrent/Frontier.java
@@ -0,0 +1,12 @@
+package buggycurrent;
+
+import battlecode.common.*;
+
+public class Frontier
+{
+    RobotController rc;
+
+    public Frontier(RobotController rc) throws GameActionException {
+        this.rc = rc;
+    }
+}
