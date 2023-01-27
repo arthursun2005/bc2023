0539f9230be65f300ff674b97655008454fb9a8d
arthur <arthur@snorlax>
2023-01-27 19:42:37 +1100

    swarm

---
+++ src/swarm/Frontier.java
@@ -0,0 +1,12 @@
+package swarm;
+
+import battlecode.common.*;
+
+public class Frontier
+{
+    RobotController rc;
+
+    public Frontier(RobotController rc) throws GameActionException {
+        this.rc = rc;
+    }
+}
