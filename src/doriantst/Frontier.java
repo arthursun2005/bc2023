82f8649d16fa77521ff05d3dc1674f0056c4ff15
ziruili <zirui.li7@gmail.com>
2023-02-01 02:25:31 +1100

    bruuuuuuuuuu

---
+++ src/doriantst/Frontier.java
@@ -0,0 +1,13 @@
+package doriantst;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class Frontier
+{
+    RobotController rc;
+
+    public Frontier(RobotController rc) throws GameActionException {
+        this.rc = rc;
+    }
+}
